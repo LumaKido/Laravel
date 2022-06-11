4d1aacf2b81508765596114563d213b1bf620f78
LumaKido <89153095+LumaKido@users.noreply.github.com>
2022-06-11 18:20:07 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('inicio');
 });
+
 Route::post('/cadastrar-produto',function(Request $request){
     produto::create([
         'nome'=>$request->nome,
@@ -25,4 +26,33 @@ Route::post('/cadastrar-produto',function(Request $request){
         'estoque'=>$request->estoque
     ]);
     echo "Produto criado com sucesso!";
+});
+
+Route::get('/listar-produto/{id}', function($id){
+    $produto = Produto::find($id);
+    return view('listar',['produto' => $produto]);
+});
+
+Route::get('/editar-produto/{id}', function($id){
+    $produto = Produto::find($id);
+    return view('editar',['produto' => $produto]);
+});
+
+Route::post('/editar-produto/{id}',function(Request $request, $id){
+    $produto = Produto::find($id);
+
+    $produto->update([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+
+    echo "Produto editado com sucesso!";
+});
+
+Route::get('/excluir-produto/{id}', function($id){
+    $produto = Produto::find($id);
+    $produto->delete();
+
+    echo "Produto excluído com sucesso!";
 });
